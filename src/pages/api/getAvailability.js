e8b65bdb1da968a1cbbec14d25e1c7f5e1ce5a05
34jasonguan <34jasonguan@gmail.com>
2024-11-07 12:21:12 -0500

    updated postgres routing for getAvailability

--- src/pages/api/getAvailability.js
+++ src/pages/api/getAvailability.js
@@ -1,4 +1,34 @@
 // pages/api/getResidents.js
+import prisma from '../../../lib/prisma';
+
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    const { netid } = req.query; 
+    try {
+      const available_dates = await prisma.availability.findMany({
+        where: {
+          netid: netid,
+        },
+        select: {
+          available_date: true,
+        },
+      });
+
+      if (available_dates.length > 0) {
+        res.status(200).json(available_dates);  
+      } else {
+        res.status(404).json({ message: 'No available dates found' });
+      }
+    } catch (error) {
+      console.error('Database query failed:', error);
+      res.status(500).json({ error: 'Database query failed', details: error.message });
+    }
+  } else {
+    res.status(405).json({ message: 'Only GET requests are allowed' });
+  }
+}
+
+/*
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
 
@@ -12,6 +42,7 @@ async function openDB() {
 export default async function handler(req, res) {
   if (req.method === 'GET') {
     const { netID } = req.query; 
+    
     try {
       const db = await openDB();
 
@@ -32,3 +63,4 @@ export default async function handler(req, res) {
     res.status(405).json({ message: 'Only GET requests are allowed' });
   }
 }
+  */
